891997754d19f2b1f13cef371c17fb118da8703d
kanan <kanansnote@gmail.com>
2024-04-30 19:41:38 +0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+// Task 1
+// let x = confirm("Səhifədən çıxırsan?");
+
+// if (x == true) {
+//     alert("Əla");
+// } else {
+//     alert("Yaxşı fikirləş");
+// }
+
+// Task 2
+
+// const MarksWeight1 = 78;
+// const MarksHeight1 = 1.69;
+
+// const MarksWeight2 = 95;
+// const MarksHeight2 = 1.88;
+
+// const JohnsWeight1 = 92;
+// const JohnsHeight1 = 1.95;
+
+// const JohnsWeight2 = 85;
+// const JohnsHeight2 = 1.76;
+
+// const MarksBMI1 = MarksWeight1 / MarksHeight1 ** 2;
+// const MarksBMI2 = MarksWeight2 / MarksHeight2 ** 2;
+
+// const JohnsBMI1 = JohnsWeight1 / JohnsHeight1 ** 2;
+// const JohnsBMI2 = JohnsWeight2 / JohnsHeight2 ** 2;
+
+// if (MarksBMI1 > JohnsBMI1) {
+//     alert(`Mark's BMI (${MarksBMI1}) is higher than John's (${JohnsBMI1})!`);
+// } else {
+//     alert(`John's BMI (${JohnsBMI1}) is higher than Mark's (${MarksBMI1})!`);
+// }
+
+// if (MarksBMI2 > JohnsBMI2) {
+//     alert(`Mark's BMI (${MarksBMI2}) is higher than John's (${JohnsBMI2})!`);
+// } else {
+//     alert(`John's BMI (${JohnsBMI2}) is higher than Mark's (${MarksBMI2})!`);
+// }
+
